57608cffe9bac7010bff59e37986c0a2b7b61306
ioana <ioana.antonia.m@gmail.ca>
2024-09-18 19:13:51 -0400

    completed lab 1

---
+++ code/labs/D0_300242429/bonjour.py
@@ -0,0 +1,5 @@
+# Student name: Ioana Marinescu
+# Student number: 300242429
+# This program displays a message
+
+print("bonjour!")
---
+++ code/labs/D0_300242429/bonjour2.py
@@ -0,0 +1,5 @@
+# Student name: Ioana Marinescu
+# Student number: 300242429
+# This program displays a message and your name
+
+print("Bonjour,", input("Quelle est votre nom? "))
